441a47b71404bcf5258cbdb6d377fd4d8b2fb6ef
Shaffan-Mohd <shaffan.mohd@maybank.com>
2026-03-05 16:21:56 +0800

    fix button build error

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -33,14 +33,27 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button";
+  ({ className, variant, size, asChild = false, children, ...props }, ref) => {
+    if (asChild) {
+      return (
+        <Slot
+          ref={ref as React.Ref<HTMLElement>}
+          className={cn(buttonVariants({ variant, size }), className)}
+          {...props}
+        >
+          {children as React.ReactElement}
+        </Slot>
+      );
+    }
+
     return (
-      <Comp
+      <button
         ref={ref}
         className={cn(buttonVariants({ variant, size }), className)}
         {...props}
-      />
+      >
+        {children}
+      </button>
     );
   }
 );
